6a8532b090274888b6475c42f4a1e3a666364971
Robert <bob.beashel@gmail.com>
2025-06-08 23:36:25 +0800

    Integrated testing set

--- main.py
+++ main.py
@@ -1,163 +1,95 @@
-
 from sklearn.model_selection import train_test_split
-from scipy.io import loadmat
 import numpy as np
-import mne
-from mne import io
-from mne.datasets import sample
 from models import EEGNet
-from tensorflow.keras import utils as np_utils
 from tensorflow.keras.callbacks import ModelCheckpoint
-from tensorflow.keras import backend as K
 from pyriemann.estimation import XdawnCovariances
 from pyriemann.tangentspace import TangentSpace
-from pyriemann.utils.viz import plot_confusion_matrix
+#from pyriemann.utils.viz import plot_confusion_matrix
 from sklearn.pipeline import make_pipeline
 from sklearn.linear_model import LogisticRegression
 from matplotlib import pyplot as plt
-
-AMPLITUDE_MAGNIFICATION = 10000 #current best = 1000
-DATASET_LOCATION = "/Volumes/My Passport/Honours_Datasets/"
-
-#Function plots 1 epoch
-def plot_epoch_with_event(epoch, sfreq, tmin=0.0, channel_names=None, title=None):
-
-    n_channels, n_times = epoch.shape
-    times = tmin + np.arange(n_times) / sfreq
-
-    offset = np.max(np.abs(epoch)) * 1.2
-    fig, ax = plt.subplots(figsize=(8, n_channels * 0.3))
-    for ch in range(n_channels):
-        ax.plot(times, epoch[ch] + ch * offset, label=(channel_names[ch] if channel_names else None))
-    # mark event at t=0
-    ax.axvline(0.0, color='k', linestyle='--', linewidth=1)
-
-    ax.set_xlabel('Time (s)')
-    ax.set_yticks(np.arange(n_channels) * offset)
-    if channel_names:
-        ax.set_yticklabels(channel_names)
-    else:
-        ax.set_yticklabels([f'Ch {i}' for i in range(n_channels)])
-    ax.set_title('Single Epoch with Event Onset (t=0)')
-    ax.grid(True, axis='x', linestyle=':', linewidth=0.5)
-    plt.tight_layout()
-    plt.title(title)
-    plt.show()
-
-def get_mne_dataset():
-    kernels, chans, samples = 1, 60, 151
-    names = ['audio left', 'audio right', 'vis left', 'vis right']
-    tmin, tmax = -0., 1
-
-    # while the default tensorflow ordering is 'channels_last' we set it here
-    # to be explicit in case if the user has changed the default ordering
-    K.set_image_data_format('channels_last')
-
-    ##################### Read the data ######################
-
-    data_path = sample.data_path()
-
-    # Set parameters and read data
-    raw_fname = str(data_path) + '/MEG/sample/sample_audvis_filt-0-40_raw.fif'
-    event_fname = str(data_path) + '/MEG/sample/sample_audvis_filt-0-40_raw-eve.fif'
+import util
+from sklearn.metrics import ConfusionMatrixDisplay, confusion_matrix
+from numpy import multiply
+from tensorflow.keras import utils as np_utils
+from pyriemann.utils.viz import plot_confusion_matrix
+#from braindecode.preprocessing import Preprocessor, exponential_moving_standardize, preprocess, create_windows_from_events
     
-    event_id = dict(aud_l=1, aud_r=2, vis_l=3, vis_r=4)
+'''
+from braindecode.datasets import MOABBDataset
 
-    # Setup for reading the raw data
-    raw = io.Raw(raw_fname, preload=True, verbose=False)
-    events = mne.read_events(event_fname)
+for i in [1,2,3,4,5,6,7,8,9]:
+    subject_id = i
+    dataset = MOABBDataset(dataset_name="BNCI2014_001", subject_ids=[subject_id])
+    
+    low_cut_hz = 4.  # low cut frequency for filtering
+    high_cut_hz = 38.  # high cut frequency for filtering
+    # Parameters for exponential moving standardization
+    factor_new = 1e-3
+    init_block_size = 1000
+    # Factor to convert from V to uV
+    factor = 1e6
 
-    ##################### Preprocess the data ######################
+    preprocessors = [
+        Preprocessor('pick_types', eeg=True, meg=False, stim=False),
+        # Keep EEG sensors
+        Preprocessor(lambda data: multiply(data, factor)),  # Convert from V to uV
+        Preprocessor('filter', l_freq=low_cut_hz, h_freq=high_cut_hz),
+        # Bandpass filter
+        Preprocessor(exponential_moving_standardize,
+                    # Exponential moving standardization
+                    factor_new=factor_new, init_block_size=init_block_size)
+    ]
 
-    raw.filter(2, None, method='iir')  # replace baselining with high-pass
+    # Transform the data
+    preprocess(dataset, preprocessors, n_jobs=-1)
 
-    raw.info['bads'] = ['MEG 2443']  # set bad channels
+    trial_start_offset_seconds = -0.5
+    # Extract sampling frequency, check that they are same in all datasets
+    sfreq = dataset.datasets[0].raw.info['sfreq']
+    assert all([ds.raw.info['sfreq'] == sfreq for ds in dataset.datasets])
 
-    picks = mne.pick_types(raw.info, meg=False, eeg=True, stim=False, eog=False, exclude='bads')
+    # Calculate the trial start offset in samples.
+    trial_start_offset_samples = int(trial_start_offset_seconds * sfreq)
 
-    ##################### Epoch, split and reshape the data ######################
+    # Create windows using braindecode function for this. It needs parameters to define how
+    # trials should be used
 
-    epochs = mne.Epochs(raw, events, event_id, tmin, tmax, proj=False, picks=picks, baseline=None, preload=True, verbose=False)
-    labels = epochs.events[:, -1] 
-    labels = np_utils.to_categorical(labels-1) # -1 Because values start at 1 rather than 0
+    n_preds_per_input = 1 #? 
+    input_window_samples = 1000
 
-    # scale due to scaling sensitivity in deep learning
-    X = epochs.get_data() * AMPLITUDE_MAGNIFICATION 
+    windows_dataset = create_windows_from_events(
+    dataset,
+    trial_start_offset_samples=trial_start_offset_samples,
+    trial_stop_offset_samples=0,
+    window_size_samples=input_window_samples,
+    window_stride_samples=n_preds_per_input,
+    drop_last_window=False,
+    preload=True
+    )
 
-    return(X, labels, chans, kernels, samples, names)
-     
-def bci_2a_helper(file_names, directory, tmin, tmax, chans):
-    all_segments = []
-    all_labels = []
-    for i, file_name in enumerate(file_names):
-        file_path = directory + file_name + ".gdf"
-        file_path_labels = directory + "true_labels_2a/" + file_name + ".mat"
-        raw = mne.io.read_raw_gdf(file_path, preload=True)
+    print(type(windows_dataset))
+    print(windows_dataset.shape)
+'''
 
-        ################################################ BANDPASS ####################################
-        
-        #raw.filter(4, 40, fir_design='firwin', skip_by_annotation='edge')
-        raw.filter(2, None, method='iir') 
 
-        ###############################################################################################
 
-        # Extract event markers
-        events, _ = mne.events_from_annotations(raw)
-        print(events)
+#########################################################################################################
 
-        # Filter ONLY relevant events
-        motor_event_ids = [7, 8, 9, 10]  # corresponds to 'left', 'right', 'foot', 'tongue'
-        mi_events = np.array([e for e in events if e[2] in motor_event_ids])
-        epochs = mne.Epochs(raw, mi_events, motor_event_ids, tmin=tmin, tmax=tmax, baseline=(None, 0), preload=True)
 
-        all_segments.append(epochs.get_data()[:,:chans,:-1])
-        print(all_segments[i].shape)
-
-        labels_raw = loadmat(file_path_labels)
-        labels_raw = labels_raw["classlabel"].reshape(-1) - 1
-        all_labels.append(labels_raw)
-
-    segments= np.concatenate(all_segments, axis=0)
-    labels = np.concatenate(all_labels, axis=0)
-
-    # scale due to scaling sensitivity in deep learning
-    segments = segments * AMPLITUDE_MAGNIFICATION
-
-    # Np array format
-    segments = np.array(segments)
-    labels = np.array(labels)
-
-    labels = np_utils.to_categorical(labels) # One hot encoding format for probabilistic classification
-
-    return segments, labels
-
-def get_bci_2a():
-    sample_rate = 250 #From BCI Dataset description
-    kernels, relevant_channels = 1, 22 # There are actually 25 channels, but we only want to retain 22, as 3 are EOG
-    tmin, tmax = -0.5, 2 # seconds before and after stimulus we want to record 
-    names        = ['left', 'right', 'foot', 'tongue']
-    directory = DATASET_LOCATION + "BCICIV_2a_gdf/"
-    file_names_training = ["A01T", "A02T", "A03T", "A05T", "A06T", "A07T", "A08T", "A09T"]
-    #file_names_testing = ["A01E", "A02E", "A03E"]
-
-    chans = relevant_channels
-
-    samples = int((tmax - (tmin)) * sample_rate)
-
-    train_segments, train_labels = bci_2a_helper(file_names_training, directory, tmin, tmax, chans)
-    #test_segments, test_labels = bci_2a_helper(file_names_testing, directory, tmin, tmax, chans) # try to implement
-
-    return(train_segments, train_labels, chans, kernels, samples, names)
-
-X, Y, chans, kernels, samples, names = get_bci_2a()
+X_train, Y_train, X_test, Y_test, chans, kernels, samples, names = util.get_bci_2a()
 
 # take 50/25/25 percent of the data to train/validate/test
-X_train, X_temp, Y_train, Y_temp = train_test_split(X, Y, test_size=0.5, stratify=Y)
+X_train, X_temp, Y_train, Y_temp = train_test_split(X_test, Y_test, test_size=0.5, stratify=Y_train)
 X_validate, X_test, Y_validate, Y_test = train_test_split(X_temp, Y_temp, test_size=0.5, stratify=Y_temp)
 
 X_train      = X_train.reshape(X_train.shape[0], chans, samples, kernels)
 X_validate   = X_validate.reshape(X_validate.shape[0], chans, samples, kernels)
 X_test       = X_test.reshape(X_test.shape[0], chans, samples, kernels)
+
+Y_train = np_utils.to_categorical(Y_train) # One hot encoding format for probabilistic classification
+Y_validate = np_utils.to_categorical(Y_validate) # One hot encoding format for probabilistic classification
+Y_test = np_utils.to_categorical(Y_test) # One hot encoding format for probabilistic classification
 
 # 2) Standardize per-channel (over time & trials)
 mean  = X_train.mean(axis = (0,2), keepdims=True)
@@ -181,7 +113,6 @@ model = EEGNet(nb_classes = 4, Chans = chans, Samples = samples,
 model.compile(loss='categorical_crossentropy', optimizer='adam', 
               metrics = ['accuracy'])
 
-# count number of parameters in the model
 numParams    = model.count_params()    
 
 # set a valid path for your system to record model checkpoints
@@ -227,9 +158,12 @@ print(model.summary())
 probs       = model.predict(X_test)
 preds       = probs.argmax(axis = -1)  
 acc         = np.mean(preds == Y_test.argmax(axis=-1))
-print("Classification accuracy: %f " % (acc))
+print("Classification accuracy EEGNet: %f " % (acc))
 
-############################# PyRiemann Portion ##############################
+
+
+
+############################# xDAWN + RG Portion ##############################
 
 # code is taken from PyRiemann's ERP sample script, which is decoding in 
 # the tangent space with a logistic regression
@@ -254,15 +188,28 @@ preds_rg     = clf.predict(X_test)
 
 # Printing the results
 acc2         = np.mean(preds_rg == Y_test.argmax(axis = -1))
-print("Classification accuracy: %f " % (acc2))
+print("Classification accuracy xDAWN + RG: %f " % (acc2))
 
 # plot the confusion matrices for both classifiers
 
 plt.figure(0)
 plot_confusion_matrix(preds, Y_test.argmax(axis = -1), names, title = 'EEGNet-8,2')
 
+
 plt.figure(1)
 plot_confusion_matrix(preds_rg, Y_test.argmax(axis = -1), names, title = 'xDAWN + RG')
 
+
+cm = confusion_matrix(Y_test.argmax(axis = -1), preds)
+disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=names)
+disp.plot()
+plt.title("EEGNet-8,2")
 plt.show()
+
+cm = confusion_matrix(Y_test.argmax(axis = -1), preds_rg)
+disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=names)
+disp.plot()
+plt.title("xDawn + RG")
+plt.show()
+
 
---
+++ util.py
@@ -0,0 +1,194 @@
+from sklearn.model_selection import train_test_split
+from scipy.io import loadmat
+import numpy as np
+import mne
+from mne import io
+from mne.datasets import sample
+from models import EEGNet
+from tensorflow.keras import utils as np_utils
+from tensorflow.keras.callbacks import ModelCheckpoint
+from tensorflow.keras import backend as K
+from pyriemann.estimation import XdawnCovariances
+from pyriemann.tangentspace import TangentSpace
+from sklearn.pipeline import make_pipeline
+from sklearn.linear_model import LogisticRegression
+from matplotlib import pyplot as plt
+from collections import Counter
+
+AMPLITUDE_MAGNIFICATION = 1000 #current best = 1000
+DATASET_LOCATION = "/Volumes/My Passport/Honours_Datasets/BCICIV_2a_gdf/"
+#DATASET_LOCATION = "/Users/bobbeashel/mne_data/MNE-bnci-data/database/data-sets/001-2014/"
+
+def get_mne_dataset():
+    kernels, chans, samples = 1, 60, 151
+    names = ['audio left', 'audio right', 'vis left', 'vis right']
+    tmin, tmax = -0., 1
+
+    # while the default tensorflow ordering is 'channels_last' we set it here
+    # to be explicit in case if the user has changed the default ordering
+    K.set_image_data_format('channels_last')
+
+    ##################### Read the data ######################
+
+    data_path = sample.data_path()
+
+    # Set parameters and read data
+    raw_fname = str(data_path) + '/MEG/sample/sample_audvis_filt-0-40_raw.fif'
+    event_fname = str(data_path) + '/MEG/sample/sample_audvis_filt-0-40_raw-eve.fif'
+    
+    event_id = dict(aud_l=1, aud_r=2, vis_l=3, vis_r=4)
+
+    # Setup for reading the raw data
+    raw = io.Raw(raw_fname, preload=True, verbose=False)
+    events = mne.read_events(event_fname)
+
+    ##################### Preprocess the data ######################
+
+    raw.filter(2, None, method='iir')  # replace baselining with high-pass
+
+    raw.info['bads'] = ['MEG 2443']  # set bad channels
+
+    picks = mne.pick_types(raw.info, meg=False, eeg=True, stim=False, eog=False, exclude='bads')
+
+    ##################### Epoch, split and reshape the data ######################
+
+    epochs = mne.Epochs(raw, events, event_id, tmin, tmax, proj=False, picks=picks, baseline=None, preload=True, verbose=False)
+    labels = epochs.events[:, -1] 
+    labels = np_utils.to_categorical(labels-1) # -1 Because values start at 1 rather than 0
+
+    # scale due to scaling sensitivity in deep learning
+    X = epochs.get_data() * AMPLITUDE_MAGNIFICATION 
+
+    return(X, labels, chans, kernels, samples, names)
+
+def bci_2a_helper(file_names, directory, tmin, tmax, chans, mode):
+    all_segments = []
+    all_labels = []
+    if mode == "gdf":
+        for i, file_name in enumerate(file_names):
+            file_path = directory + file_name + ".gdf"
+            file_path_labels = directory + "true_labels/" + file_name + ".mat"
+            raw = mne.io.read_raw_gdf(file_path, preload=True)
+
+            ################################################ BANDPASS ####################################
+            
+            raw.filter(4, 40, fir_design='firwin', skip_by_annotation='edge')
+            #raw.filter(2, None, method='iir') 
+
+            ###############################################################################################
+
+            events, event_id = mne.events_from_annotations(raw, event_id = {
+                '769': 1,   # left hand
+                '770': 2,   # right hand
+                '771': 3,   # foot
+                '772': 4,   # tongue
+                '783': 5    #unknown (eval sets)
+            })
+
+            epochs = mne.Epochs(raw, events, tmin=tmin, tmax=tmax, baseline=None, preload=True)
+
+            all_segments.append(epochs.get_data()[:,:chans,:-1]) # i did this -1 because the samples was always exactly 1 too high.
+            print(all_segments[i].shape)
+
+            labels_raw = loadmat(file_path_labels)
+            labels_raw = labels_raw["classlabel"].reshape(-1) - 1 #Change from 1,2,3,4 to 0,1,2,3 because the EEGNet model likes it
+            all_labels.append(labels_raw)
+
+    elif mode == "mat":
+        
+        for i, file_name in enumerate(file_names):
+
+            ####################### GET FILE #######################
+            print("\n", file_name)
+            path = directory + file_name + ".mat"
+            data_dict = loadmat(path)
+            data = data_dict['data'].squeeze()[3:]# First 3 are EOG calibration, not MI
+            
+            k, cum_time = 0,0
+            trials_list = []
+            for t in data:             
+                k += 1
+                cum_time += t['X'][0,0].shape[0]
+                trials = t['trial'][0, 0][:,0]
+                adjusted_trials = trials + cum_time
+                trials_list.append(adjusted_trials)
+            trials = np.concatenate(trials_list)
+
+            X_list = [t['X'][0, 0][:, :22] for t in data]
+            X = np.concatenate(X_list, axis=0)
+
+            sample_rate = data[0]['fs'][0][0][0][0]
+
+            y_list_i = [t['y'][0, 0][:,0] for t in data]
+            y_i = np.concatenate(y_list_i)
+            y_i = y_i - 1
+
+            raw = mne.io.RawArray(X.T, mne.create_info(ch_names=[f"EEG{j+1}" for j in range(X.shape[1])], sfreq=sample_rate, ch_types="eeg"))
+            raw.filter(4, 40, method='iir') 
+
+            events = np.column_stack([trials,np.zeros_like(trials), y_i])
+            epochs = mne.Epochs(raw, events, tmin=tmin, tmax=tmax, baseline=None, preload=True, reject_by_annotation=False)
+            print(Counter(tuple(r) for r in epochs.drop_log))
+
+            all_segments.append(epochs.get_data()[:,:chans,:-1])
+            all_labels.append(epochs.events[:, 2].astype(int))
+            print(all_segments[i].shape)
+
+    labels = np.concatenate(all_labels, axis=0)
+    
+    segments= np.concatenate(all_segments, axis=0)
+    segments = segments * AMPLITUDE_MAGNIFICATION
+
+    # Np array format
+    segments = np.array(segments)
+    labels = np.array(labels)
+    print(segments.shape)
+    print("LABELS: ", labels)
+
+    
+
+    return segments, labels
+
+def get_bci_2a():
+    sample_rate = 250 #From BCI Dataset description
+    kernels, relevant_channels = 1, 22 # There are actually 25 channels, but we only want to retain 22, as 3 are EOG
+    tmin, tmax = 0.5, 2.5 # seconds before and after stimulus we want to record 
+    names        = ['left', 'right', 'foot', 'tongue']
+    directory = DATASET_LOCATION
+    file_names_training = ["A01T", "A02T", "A03T","A04T", "A05T", "A06T", "A07T", "A08T", "A09T"]
+    file_names_testing = ["A01E", "A02E", "A03E", "A04E", "A05E", "A06E", "A07E", "A08E", "A09E"]
+
+    chans = relevant_channels
+
+    samples = int((tmax - (tmin)) * sample_rate)
+
+    train_segments, train_labels = bci_2a_helper(file_names_training, directory, tmin, tmax, chans, mode="gdf")
+    test_segments, test_labels = bci_2a_helper(file_names_testing, directory, tmin, tmax, chans, mode="gdf")
+
+    return(train_segments, train_labels, test_segments, test_labels, chans, kernels, samples, names)
+
+
+#Function plots 1 epoch
+def plot_epoch_with_event(epoch, sfreq, tmin=0.0, channel_names=None, title=None):
+
+    n_channels, n_times = epoch.shape
+    times = tmin + np.arange(n_times) / sfreq
+
+    offset = np.max(np.abs(epoch)) * 1.2
+    fig, ax = plt.subplots(figsize=(8, n_channels * 0.3))
+    for ch in range(n_channels):
+        ax.plot(times, epoch[ch] + ch * offset, label=(channel_names[ch] if channel_names else None))
+    # mark event at t=0
+    ax.axvline(0.0, color='k', linestyle='--', linewidth=1)
+
+    ax.set_xlabel('Time (s)')
+    ax.set_yticks(np.arange(n_channels) * offset)
+    if channel_names:
+        ax.set_yticklabels(channel_names)
+    else:
+        ax.set_yticklabels([f'Ch {i}' for i in range(n_channels)])
+    ax.set_title('Single Epoch with Event Onset (t=0)')
+    ax.grid(True, axis='x', linestyle=':', linewidth=0.5)
+    plt.tight_layout()
+    plt.title(title)
+    plt.show()
